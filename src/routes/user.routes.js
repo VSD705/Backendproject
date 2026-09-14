2fd22aa2eedb05cca52d49ba529beaefd31a94f9
vedantdhamane <vedantdhamane2005@gmail.com>
2026-09-14 20:43:01 +0530

    login logout

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -17,6 +17,11 @@ router.route("/register").post(
     }
 ]), 
     registerUser
-);
+)
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
 
 export default router;
